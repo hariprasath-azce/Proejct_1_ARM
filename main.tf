e244447a6b887e4fdd617ee1b196e3d71b45bae8
hariprasath-azce <hariprasathd96@gmail.com>
2024-12-04 23:27:43 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -10,9 +10,6 @@ resource "azurerm_virtual_network" "vnet" {
   location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
   address_space       = ["10.0.0.0/16"]
-
-  # Ensure VNet waits for Resource Group
-  depends_on = [azurerm_resource_group.rg]
 }
 
 # Subnet
@@ -22,13 +19,79 @@ resource "azurerm_subnet" "subnet" {
   virtual_network_name = azurerm_virtual_network.vnet.name
   address_prefixes     = ["10.0.1.0/24"]
 
-  # Add service endpoint for Microsoft.Storage
   service_endpoints = ["Microsoft.Storage"]
 }
 
+# Network Security Group
+resource "azurerm_network_security_group" "nsg" {
+  name                = "Project1-nsg"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+
+  security_rule {
+    name                       = "AllowSSH"
+    priority                   = 1001
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "22"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
+}
+
+# Public IP
+resource "azurerm_public_ip" "public_ip" {
+  name                = "Project1-public-ip"
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = azurerm_resource_group.rg.location
+  allocation_method   = "Static"
+  sku                 = "Standard"
+}
+
+# Network Interface
+resource "azurerm_network_interface" "nic" {
+  name                = "Project1-nic"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+
+  ip_configuration {
+    name                          = "internal"
+    subnet_id                     = azurerm_subnet.subnet.id
+    private_ip_address_allocation = "Dynamic"
+    public_ip_address_id          = azurerm_public_ip.public_ip.id
+  }
+}
+
+# Virtual Machine
+resource "azurerm_linux_virtual_machine" "vm" {
+  name                = "Project1-vm"
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = azurerm_resource_group.rg.location
+  size                = "Standard_B1s"
+  network_interface_ids = [azurerm_network_interface.nic.id]
+
+  os_disk {
+    caching              = "ReadWrite"
+    storage_account_type = "Standard_LRS"
+  }
+
+  source_image_reference {
+    publisher = "Canonical"
+    offer     = "UbuntuServer"
+    sku       = "18.04-LTS"
+    version   = "latest"
+  }
+
+  computer_name  = "Project1-vm"
+  admin_username = "azureuser"
+  admin_password = "YourPassword1!" # Replace with a secure password
+}
+
 # Storage Account
-resource "azurerm_storage_account" "storage" {
-  name                     = "storageaccountproj1hd123" # Must be globally unique
+resource "azurerm_storage_account" "example" {
+  name                     = "storageaccountproj1hd123"
   resource_group_name      = azurerm_resource_group.rg.name
   location                 = azurerm_resource_group.rg.location
   account_tier             = "Standard"

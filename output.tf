2050daedceb7193ea3b9f826cc9c4cbbb8413fad
hariprasath-azce <hariprasathd96@gmail.com>
2024-12-04 23:27:27 -0500

    Update output.tf

--- output.tf
+++ output.tf
@@ -1,39 +1,34 @@
-# Resource Group Name
+# Output: Resource Group Name
 output "resource_group_name" {
   value = azurerm_resource_group.rg.name
 }
 
-# Resource Group Location
-output "resource_group_location" {
-  value = azurerm_resource_group.rg.location
-}
-
-# Virtual Network Name
-output "virtual_network_name" {
+# Output: Virtual Network Name
+output "vnet_name" {
   value = azurerm_virtual_network.vnet.name
 }
 
-# Subnet ID
+# Output: Subnet ID
 output "subnet_id" {
   value = azurerm_subnet.subnet.id
 }
 
-# Virtual Machine Private IP Address
+# Output: Virtual Machine Private IP
 output "vm_private_ip" {
   value = azurerm_linux_virtual_machine.vm.private_ip_address
 }
 
-# Virtual Machine Public IP Address
+# Output: Virtual Machine Public IP
 output "vm_public_ip" {
   value = azurerm_public_ip.public_ip.ip_address
 }
 
-# Storage Account Name
+# Output: Storage Account Name
 output "storage_account_name" {
-  value = azurerm_storage_account.storage.name
+  value = azurerm_storage_account.example.name
 }
 
-# Storage Account ID
+# Output: Storage Account ID
 output "storage_account_id" {
-  value = azurerm_storage_account.storage.id
+  value = azurerm_storage_account.example.id
 }

423a2d4bf4f4259cf2798388781df3d7a836a2d9
hariprasath-azce <hariprasathd96@gmail.com>
2024-12-06 15:42:06 -0500

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,15 +1,4 @@
-terraform {
-  required_providers {
-    azurerm = {
-      source  = "hashicorp/azurerm"
-      version = ">= 3.0"
-    }
-  }
-}
-
 provider "azurerm" {
   features {}
 
-  subscription_id = "2a1f765f-c81a-419f-a920-25a2c9e38966"
-  tenant_id       = "49bba7a4-424b-4070-a70e-886e9dd7caef"
-}
+  }
